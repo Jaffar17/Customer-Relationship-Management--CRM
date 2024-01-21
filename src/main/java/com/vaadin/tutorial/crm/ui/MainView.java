e0bdc806073c3d8fc616510270d72eda32079d40
Jaffar Abbas <abbasjaffar28@gmail.com>
2024-01-21 23:05:53 +0500

    hooked the view to backend and populated test data

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -4,21 +4,32 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 @Route("")
 public class MainView extends VerticalLayout {
+    private ContactService contactService;
     private Grid<Contact> grid = new Grid<>(Contact.class);
 
-    public MainView() {
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+
         addClassName("list-view");
         setSizeFull();
+
         configureGrid();
         add(grid);
+
+        updateList();
     }
 
     private void configureGrid() {
         grid.addClassName("contact-grid");
         grid.setSizeFull();
         grid.setColumns("firstName", "lastName", "email", "status");
+    }
+
+    private void updateList(){
+        grid.setItems(contactService.findAll());
     }
 }
