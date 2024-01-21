235b568959bd79f192f067f0901cf09fea1d599d
Jaffar Abbas <abbasjaffar28@gmail.com>
2024-01-21 23:00:24 +0500

    added and configured grid for contacts

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,10 +1,24 @@
 package com.vaadin.tutorial.crm.ui;
+
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.tutorial.crm.backend.entity.Contact;
 
 @Route("")
 public class MainView extends VerticalLayout {
-    public MainView() {
+    private Grid<Contact> grid = new Grid<>(Contact.class);
 
+    public MainView() {
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
+    }
+
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
     }
 }
