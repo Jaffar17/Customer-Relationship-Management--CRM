9cbdfbd4547c61e7aff20e69feac0f6fa5441afe
Jaffar Abbas <abbasjaffar28@gmail.com>
2024-01-27 22:29:17 +0500

    added code to make contact form reusable

--- src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
@@ -8,8 +8,12 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
+
+import java.util.List;
 
 public class ContactForm extends FormLayout {
     TextField firstName = new TextField("First name");
@@ -21,8 +25,16 @@ public class ContactForm extends FormLayout {
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
 
-    public ContactForm() {
+    Binder<Contact> binder = new BeanValidationBinder<Contact>(Contact.class);
+
+    public ContactForm(List<Company> companies) {
         addClassName("contact-form");
+        binder.bindInstanceFields(this);
+
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+        status.setItems(Contact.Status.values());
+
         add(firstName,
                 lastName,
                 email,
